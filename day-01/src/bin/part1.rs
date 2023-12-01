231ddd9f074f82f6966d8a34f604604b0002151d
Brent de Kok <brent.dekok@coolblue.nl>
2023-12-01 21:42:07 +0100

    [Day 1] first Rust thing ever

---
+++ day-01/src/bin/part1.rs
@@ -0,0 +1,30 @@
+fn main() {
+    let input = include_str!("./input.txt");
+    println!("{:?}", part_one(input));
+}
+
+fn part_one(input: &str) -> isize {
+    return input
+        .split_whitespace()
+        .map(|line| {
+            let numbers: Vec<_> = line.chars().filter_map(|sign| sign.to_digit(10)).collect();
+            let first_and_last: String = numbers.first().unwrap().to_string() + &numbers.last().unwrap().to_string();
+
+            return first_and_last.parse::<isize>().unwrap()
+        })
+        .sum();
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::part_one;
+
+    #[test]
+    fn day_one_part_one() {
+        let test_input = include_str!("./part1-test.txt");
+
+        let expected = 142;
+        let result = part_one(test_input);
+        assert_eq!(result, expected);
+    }
+}
---
+++ day-01/src/bin/part2.rs
@@ -0,0 +1,41 @@
+fn main() {
+    let input = include_str!("./input.txt");
+    println!("{:?}", part_two(input));
+}
+
+fn part_two(input: &str) -> isize {
+    return input
+        .split_whitespace()
+        .map(|line| {
+            let newlines = line
+                .replace("one", "one1one")
+                .replace("two", "two2two")
+                .replace("three", "three3three")
+                .replace("four", "four4four")
+                .replace("five", "five5five")
+                .replace("six", "six6six")
+                .replace("seven", "seven7seven")
+                .replace("eight", "eight8eight")
+                .replace("nine", "nine9nine");
+
+            let numbers: Vec<_> = newlines.chars().filter_map(|sign| sign.to_digit(10)).collect();
+            let first_and_last: String = numbers.first().unwrap().to_string() + &numbers.last().unwrap().to_string();
+
+            return first_and_last.parse::<isize>().unwrap()
+        })
+        .sum();
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::part_two;
+
+    #[test]
+    fn day_one_part_two() {
+        let test_input = include_str!("./part2-test.txt");
+
+        let expected = 281;
+        let result = part_two(test_input);
+        assert_eq!(result, expected);
+    }
+}
