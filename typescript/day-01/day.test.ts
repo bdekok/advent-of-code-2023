55a370d991370a6d3912b944d033075027d6ec0b
Brent de Kok <brent.dekok@coolblue.nl>
2023-12-07 07:56:20 +0100

    [Chore] clean up

--- typescript/day-01/day.test.ts
+++ typescript/day-01/day.test.ts
@@ -1,23 +1,22 @@
 import { assertEquals } from "../depts.ts";
+import { getTestInputOfCurrentDay } from "../utils.ts";
 import { exerciseOne, exerciseTwo } from "./day.ts";
 
-Deno.test("Exercise one combines the first digit and the last digit to form a single two-digit number and then sums the lines.", () => {
-  const testData = `1abc2
-  pqr3stu8vwx
-  a1b2c3d4e5f
-  treb7uchet`
+Deno.test("Exercise one",async () => {
+  const testData = await getTestInputOfCurrentDay(import.meta.url);
   const expected = 142;
   assertEquals(exerciseOne(testData), expected);
 });
 
-Deno.test("Exercise two combines the first digit  and the last digit to form a single two-digit number and then sums the lines. The digits can be written out in text like 'one'", () => {
+Deno.test("Exercise two", () => {
   const testData = `two1nine
   eightwothree
   abcone2threexyz
   xtwone3four
   4nineeightseven2
   zoneight234
-  7pqrstsixteen`
+  7pqrstsixteen
+  `
   const expected = 281;
   assertEquals(exerciseTwo(testData), expected);
 });
