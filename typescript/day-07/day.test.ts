87f7dd1a1145ba81e2a31b8bb72e8d06b9dafd2e
Brent de Kok <brent.dekok@coolblue.nl>
2023-12-07 21:45:22 +0100

    [Day 7] exercise two

--- typescript/day-07/day.test.ts
+++ typescript/day-07/day.test.ts
@@ -1,22 +1,61 @@
 import { assertEquals } from "../depts.ts";
 import { getTestInputOfCurrentDay } from "../utils.ts";
-import { exerciseOne } from "./day.ts";
+import { exerciseOne, exerciseTwo } from "./day.ts";
 
-Deno.test("Exercise one",async () => {
+Deno.test("Exercise one", async () => {
   const testData = await getTestInputOfCurrentDay(import.meta.url);
   const expected = 6440;
   assertEquals(exerciseOne(testData), expected);
 });
 
-// Deno.test("Exercise two", () => {
-//   const testData = `two1nine
-//   eightwothree
-//   abcone2threexyz
-//   xtwone3four
-//   4nineeightseven2
-//   zoneight234
-//   7pqrstsixteen
-//   `
-//   const expected = 281;
-//   assertEquals(exerciseTwo(testData), expected);
-// });
+Deno.test("Exercise two", async () => {
+  const testData = await getTestInputOfCurrentDay(import.meta.url);
+  const expected = 5905;
+  assertEquals(exerciseTwo(testData), expected);
+});
+
+Deno.test("Exercise two - extra test all J's", async() => {
+  const testData = await getTestInputOfCurrentDay(import.meta.url, 'input-test-2.txt');
+
+  //  2 pair, 4 of a kind 5 of a kind,
+  // so 1 * 28 + 2*220 + 3*483 = 1917
+  const expected = 1917;
+  assertEquals(exerciseTwo(testData), expected);
+});
+
+Deno.test("Exercise two - extra test one all J, one 4 J's and one other ", async() => {
+  /*
+  4JJJJ 483 - 5 of a kind rank 4
+  JJJJJ 5   - 5 of a kind rank 3
+  KTJJT 220 - 4 of a kind rank 2
+  KK677 28  - 2 pair      rank 1
+
+  = 4*483 + 3*5 + 2*220 + 1*28 = 2415
+  */
+  const testData = await getTestInputOfCurrentDay(import.meta.url, 'input-test-3.txt');
+  const expected = 2415;
+  assertEquals(exerciseTwo(testData), expected);
+})
+
+Deno.test("Three of a kind tests", async() => {
+  /*
+  4LJKJ 483 - three of a kind   rank 4
+  J263J 220 - Three of a kind   rank 3
+  JJA34 1   - Three of a kind   rank 2
+  KK677 28  - two pair          rank 1
+
+  = 4*483 + 3*220 + 2*1 + 1*28 = 2622
+  */
+
+  const testData = await getTestInputOfCurrentDay(import.meta.url, 'input-test-4.txt');
+  const expected = 2622;
+  assertEquals(exerciseTwo(testData), expected);
+
+})
+
+
+Deno.test('exercise two - extra test 4 thank you random internet person it were the full houses', async() => {
+  const testData = await getTestInputOfCurrentDay(import.meta.url, 'input-test-5.txt');
+  const expected = 6839;
+  assertEquals(exerciseTwo(testData), expected);
+})
--- typescript/day-07/day.ts
+++ typescript/day-07/day.ts
@@ -21,102 +21,121 @@ const cardTypes = {
 
 type CardType = (typeof cardTypes)[keyof typeof cardTypes];
 
-const cardScores: Record<string, number>= {
-  'A': 14,
-  'K': 13,
-  'Q': 12,
-  'J': 11,
-  'T': 10,
-  '9': 9,
-  '8': 8,
-  '7': 7,
-  '6': 6,
-  '5': 5,
-  '4': 4,
-  '3': 3,
-  '2': 2,
-}
+const cardScores: Record<string, number> = {
+  A: 14,
+  K: 13,
+  Q: 12,
+  J: 11,
+  T: 10,
+  "9": 9,
+  "8": 8,
+  "7": 7,
+  "6": 6,
+  "5": 5,
+  "4": 4,
+  "3": 3,
+  "2": 2,
+};
 
-// type CardScorekey = keyof typeof cardScores;
-// type CardScore = (typeof cardScores)[keyof typeof cardScores];
+const cardScoresWithJokers: Record<string, number> = {
+  ...cardScores,
+  J: 1,
+};
 
 type Card = {
   roll: string[];
   type: CardType;
 };
 
-const getNumberCount = (roll: string[]): number[] => {
-  return Object.values(
-    roll.toSorted().reduce((acc, currentNumber, index, array) => {
-      const nextNumber = array[index + 1];
-      if (!acc[currentNumber]) {
-        acc[currentNumber] = 1;
-      }
-      if (currentNumber === nextNumber) {
-        acc[currentNumber] = acc[currentNumber] + 1;
-      }
-      return acc;
-    }, {} as Record<string, number>)
+const getNumberCount = (roll: string[]): Record<string, number> => {
+  return roll.toSorted().reduce((acc, currentNumber, index, array) => {
+    const nextNumber = array[index + 1];
+    if (!acc[currentNumber]) {
+      acc[currentNumber] = 1;
+    }
+    if (currentNumber === nextNumber) {
+      acc[currentNumber] = acc[currentNumber] + 1;
+    }
+    return acc;
+  }, {} as Record<string, number>);
+};
+
+const hasXOfAKind = (
+  numberCount: Record<string, number>,
+  x: number,
+  withJokers: boolean
+): boolean => {
+  for (const index in numberCount) {
+    const count = numberCount[index];
+
+    if (withJokers && index !== "J" && (count === x || count + numberCount.J === x)) {
+      return true;
+    }
+
+    if (count === x) {
+      return true;
+    }
+  }
+  return false;
+};
+
+const hasTwoPair = (numberCount: Record<string, number>): boolean => {
+  return (
+    Object.values(numberCount).filter((count) => {
+      return count === 2;
+    }).length === 2
   );
 };
 
-const hasXOfAKind = (numberCount: number[], x: number): boolean => {
-  return numberCount.some((count) => count === x);
-};
-
-const hasTwoPair = (numberCount: number[]): boolean => {
-  const result = numberCount.filter((count) => count === 2);
-  console.log({ result, numberCount });
-
-  return result.length === 2;
-};
-
-const getCard = (roll: string[]): Card => {
+function isFullHouse(numberCount: Record<string, number>, withJokers: boolean): boolean {
+  const uniqueValues = new Set(Object.keys(numberCount));
+  return uniqueValues.size === 2 || withJokers && (uniqueValues.size === 3 && uniqueValues.has("J"));
+}
+const getCard = (roll: string[], withJokers: boolean): Card => {
   const numberCount = getNumberCount(roll);
-  let type: CardType = cardTypes.highCard;
 
-  if (hasXOfAKind(numberCount, 5)) {
+  if (hasXOfAKind(numberCount, 5, withJokers)) {
     return { roll, type: cardTypes.fiveOfAKind };
   }
-  if (hasXOfAKind(numberCount, 4)) {
+  if (hasXOfAKind(numberCount, 4, withJokers)) {
     return { roll, type: cardTypes.fourOfAKind };
   }
-  if (hasXOfAKind(numberCount, 3) && hasXOfAKind(numberCount, 2)) {
+  if (isFullHouse(numberCount, withJokers)) {
     return { roll, type: cardTypes.fullHouse };
   }
-  if (hasXOfAKind(numberCount, 3)) {
+  if (hasXOfAKind(numberCount, 3, withJokers)) {
     return { roll, type: cardTypes.threeOfAKind };
   }
   if (hasTwoPair(numberCount)) {
     return { roll, type: cardTypes.twoPairs };
   }
-  if (hasXOfAKind(numberCount, 2)) {
+  if (hasXOfAKind(numberCount, 2, withJokers)) {
     return { roll, type: cardTypes.pair };
   }
-  return {
-    roll,
-    type,
-  };
+  return { roll, type: cardTypes.highCard };
 };
 
-export function exerciseOne(input: string): number {
-  const result = input
+type GetTotalWinningsArgs = {
+  input: string;
+  hasJokers: boolean;
+};
+
+function getTotalWinnings({ input, hasJokers }: GetTotalWinningsArgs): number {
+  const cardScore = hasJokers ? cardScoresWithJokers : cardScores;
+
+  return input
     .trim()
     .split("\n")
     .map((row: string) => {
-      const [cards, score] = row.split(" ");
-      const card = getCard(cards.split(""));
-      return {
-        card,
-        score,
-      };
+      const [cards, bid] = row.split(" ");
+      const card = getCard(cards.split(""), hasJokers);
+      return { card, bid };
     })
     .toSorted((a, b) => {
       if (a.card.type === b.card.type) {
         for (let i = 0; i < a.card.roll.length; i++) {
-
-          const cardScoreA = cardScores[a.card.roll[i]]
-          const cardScoreB = cardScores[b.card.roll[i]]
+          const cardScoreA = cardScore[a.card.roll[i]];
+          const cardScoreB = cardScore[b.card.roll[i]];
           if (cardScoreA === cardScoreB) {
             continue;
           }
@@ -124,20 +143,15 @@ export function exerciseOne(input: string): number {
         }
       }
       return b.card.type - a.card.type;
-    }).map((x, index, a) => {
-      return {
-        ...x,
-        rank: a.length - index
-      };
-    }).toReversed()
-    
-  
-
-  console.log({result})
-  // console.log(getNumberCount(["a", "2", "b", "1", "2"]));
-  return result.reduce((acc, { score }, index) => {
-    return acc + parseInt(score) * (index + 1);
-  }, 0);
+    })
+    .map((x, index, a) => ({ ...x, rank: a.length - index }))
+    .reduce((acc, { bid, rank }) => acc + parseInt(bid) * rank, 0);
 }
 
-export function exerciseTwo(input: string): number {}
+export function exerciseOne(input: string): number {
+  return getTotalWinnings({ input, hasJokers: false });
+}
+
+export function exerciseTwo(input: string): number {
+  return getTotalWinnings({ input, hasJokers: true });
+}
